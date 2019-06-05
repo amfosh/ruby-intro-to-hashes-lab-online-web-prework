b5d5dfe9d379e8a84d40c5e1c3d5dedc797ca4cc
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-05 21:05:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,10 +29,10 @@ def monopoly_with_third_tier
     monopoly= {
       :rent_in_dollars=>
       {
-        :one_piece=>25,
-        :two_pieces=>50,
-        :three_pieces=>100,
-        :four_pieces=>200
+        :one_piece_owned=>25,
+        :two_pieces_owned=>50,
+        :three_piece_owned=>100,
+        :four_pieces_owned=>200
       },
     :names=>
       {
