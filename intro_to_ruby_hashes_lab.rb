9d858734ed4b2b4b31c09aa8681de94bbdbb0eb9
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-05 20:43:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,13 +17,14 @@ end
 def monopoly_with_second_tier
   monopoly= {
     :railroads=>{}
+  }
 end
 
 def monopoly_with_third_tier
     monopoly= {
       :railroads=>
       {
-        :pieces=>4,
+        :pieces=>"4",
         :name=>{}
     }
 end
